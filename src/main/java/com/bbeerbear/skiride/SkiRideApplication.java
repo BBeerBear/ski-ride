7380725e5918097f1f01f076794a8280da4db6a5
Limin <xiongx659@gmail.com>
2023-03-25 22:11:47 -0400

    Initial commit

---
+++ src/main/java/com/bbeerbear/skiride/SkiRideApplication.java
@@ -0,0 +1,13 @@
+package com.bbeerbear.skiride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkiRideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbeerbear/skiride/SkiRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbeerbear.skiride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
